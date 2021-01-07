6a5d2d166e00cbe337c8451efce5a75761d3f0f0
yunjiaen <2591514808@qq.com>
2021-01-07 18:32:03 +0800

    修改

---
+++ src/org/neuedu/ch类和对象/practice07/Person.java
@@ -0,0 +1,44 @@
+package org.neuedu.ch类和对象.practice07;
+
+public class Person {
+    String name;
+    int age;
+    double height;
+
+    public Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void sayHello() {
+        System.out.println("my name is "+name);
+    }
+}
